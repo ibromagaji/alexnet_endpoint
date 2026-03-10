e7e6098b40a3ac7c9c563733200f29d26b005e95
ibromagaji <ibrahimdahirumagajii@gmail.com>
2026-03-10 02:49:49 +0100

    Update alexnet_api.py

--- alexnet_api.py
+++ alexnet_api.py
@@ -27,7 +27,7 @@ S3_BUCKET_NAME = "alexnet-weights"
 S3_MODEL_KEY = "models/best_model.ckpt"  # e.g., "models/random_forest.pkl"
 LOCAL_MODEL_PATH = "/tmp/best_model.ckpt" # Store temporarily
 S3_BUCKET_CLASSNAMES_NAME = 'alexnet-class-names'
-S3_CLASSNAME_FILE = '//class_names.json'
+S3_CLASSNAME_FILE = 'class_names.json'
 LOCAL_CLASSNAME_FILE = '/tmp/class_names.json'
 
 
